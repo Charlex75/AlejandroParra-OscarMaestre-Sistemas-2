b6a683181eba64844f346018bb364c3b0b785e3d
Charlex75 <alex.parra.purroy@gmail.com>
2022-12-29 21:04:24 +0100

    includes

---
+++ encuentraprimos.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
